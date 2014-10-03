b1cbcd330973fe7ab81828cbd0e13cf7e9e15606
Scott Coleman <scoleman@MC68985-scoleman.local>
2014-10-03 10:42:44 -0700

    Bug 2781 Fixed problem with absolute path in buildAllSim.sh

--- buildAllSim.sh
+++ buildAllSim.sh
@@ -134,7 +134,7 @@ else
         # Due to a problem with the newer os-x tools the simulator (not the device) build fails the fips signature check
         # For now we'll disable this check (for the simulator only)
         # Note that this breaks fips compiliance for the simulator but we're still fips compliant with the actual device build
-         sed -ie 's/FIPS_check_incore_fingerprint(void)/FIPS_check_incore_fingerprint(void) {return 1;}  dummy(void)/' "/Users/scoleman/dev/IOSFipsBuilds/test1001/fcids/devSim/$FIPS_BASE/fips/fips.c"
+         sed -ie 's/FIPS_check_incore_fingerprint(void)/FIPS_check_incore_fingerprint(void) {return 1;}  int dummy(void)/' "./fips/fips.c"
         
 
         ./config fipscanisterbuild >> $LOGFILE 2>&1
