e289f1d9b1fb6d57b3f7747679aa64eb819a757f
Scott Coleman <scoleman@MC68985-scoleman.local>
2015-07-21 15:41:25 -0700

    Bug 2781 Fixed monir problem and added multiple passes to unit tests

--- test/fips-pi/fips-pi/ViewController.m
+++ test/fips-pi/fips-pi/ViewController.m
@@ -47,12 +47,14 @@ extern unsigned int         FIPS_incore_fingerprint (unsigned char *, unsigned i
 int testPassCount = 0;
 int testFailCount = 0;
 int testTotalCount = 0;
-int iterations = 0;
+int iterations = 1;
 BOOL databaseTestsPassed = false;
 NSString *testDescription = @"";
 int retVal;
 
-const int NUM_ITERATIONS = 1;
+// -------------------------------------------------------
+// Set the number of test iterations to perform here
+const int NUM_ITERATIONS_TO_PERFORM = 10;
 
 
 @interface ViewController ()
@@ -70,12 +72,20 @@ const int NUM_ITERATIONS = 1;
 @synthesize m_mainView;
 
 
-
+/*!
+ * @brief Starts a test case
+ * @param testDescription descriptioni of test
+ */
 NSString *startTest(NSString *testDescription) {
     NSLog(@"%@  ------ Starting test ",testDescription);
     return testDescription;
 }
 
+/*!
+ * @brief Performs test case pass/fail
+ * @param result (boolean) Condition being tested
+ * @param testDescription descriptioni of test
+ */
 void assertT2Test(bool result, NSString *testDescription) {
     testTotalCount++;
     if (result) {
@@ -90,10 +100,11 @@ void assertT2Test(bool result, NSString *testDescription) {
     }
 }
 
-
-
-
-
+/*!
+ * @brief Displays an error message on the UI
+ * @param msg Message to display
+ * @param msg Error code
+ */
 void DisplayErrorMessage(const char* msg, unsigned long err)
 {
     if(!msg)
@@ -166,11 +177,6 @@ void DisplayErrorMessage(const char* msg, unsigned long err)
         mode = FIPS_mode();
         FIPS_ASSERT((0 != mode && YES == state) || (0 == mode && NO == state));
     }
-    
-
-    
-    
-
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -239,116 +245,148 @@ void DisplayErrorMessage(const char* msg, unsigned long err)
     
     // Start Unit Tests:
     
-    iterations++;
-//		while(iterations++ < NUM_ITERATIONS) {
-    
-    
-    // ------------------------------------------
-    // Test 1 - Test we can enter/edit FIP mode
-    // ------------------------------------------
-    testDescription = startTest(@"Test 1.0 Confirm FIP mode");
-    retVal = FIPS_mode_set(1 /*on*/);
-    int fipsMode = FIPS_mode();
-    assertT2Test(retVal == T2True, testDescription );
-    assertT2Test(fipsMode == T2FipsModeOn, testDescription );
-    
-    testDescription = startTest(@"Test 1.1 Confirm FIP mode");
-    retVal = FIPS_mode_set(0 /*off*/);
-    fipsMode = FIPS_mode();
-    assertT2Test(retVal == T2True, testDescription );
-    assertT2Test(fipsMode == T2FipsModeOff, testDescription );
-    
-    testDescription = startTest(@"Test 1.2 Confirm FIP mode");
-    retVal = FIPS_mode_set(1 /*on*/);
-    fipsMode = FIPS_mode();
-    assertT2Test(retVal == T2True, testDescription );
-    assertT2Test(fipsMode == T2FipsModeOn, testDescription );
-    
-    
-    
-    // Make sure that the UI switch agrees with the current FIP mode
-    const int mode = FIPS_mode();
-    const BOOL state = mode ? YES : NO;
-    [m_modeSwitch setOn:state];
 
-    // ------------------------------------------
-    // Test 2 - Test low level encrypt routines (AES_encrypt....
-    // ------------------------------------------
-    testLowLevelEncryptDecrypt();
+    while(true) {
     
-    // ------------------------------------------
-    // Test 3,4 - Test raw string encrypt/descypt
-    // ------------------------------------------
+        FIPS_mode_set(0 /*off*/);
     
-    // Note that we intintially don't call initializeCrypto() here - these routines are stand alone
-    // and need no initialization
-    testDescription = startTest(@"Test 3.0 Test raw string encrypt/decrypt");
-    NSString *plainText = @"This is a test string to encrypt";
-    NSString *pin = (@"This is a funky $$$ pin");
-    
-    NSString *encryptedText = encryptRaw(pin, plainText);
-    NSString *decryptedText = decryptRaw(pin, encryptedText);
-    assertT2Test(![encryptedText isEqualToString:plainText], testDescription ); // Make sure encryped is different
-    assertT2Test([decryptedText isEqualToString:plainText], testDescription );  // Make sure reconstructed is same
-    
-    testDescription = startTest(@"Test 4.0 Test raw string encrypt/decrypt - blank string, blank pin");
-    plainText = @"";
-    pin = (@"");
-    
-    encryptedText = encryptRaw(pin, plainText);
-    decryptedText = decryptRaw(pin, encryptedText);
-    assertT2Test(![encryptedText isEqualToString:plainText], testDescription ); // Make sure encryped is different
-    assertT2Test([decryptedText isEqualToString:plainText], testDescription ); // Make sure reconstructed is same
-    
-    
-    // ------------------------------------------
-    // Test 5/6 - Test raw binary encrypt/decrypt
-    // ------------------------------------------
-    testDescription = startTest(@"Test 5,6 Test raw binary encrypt/decrypt");
-    pin = @"password";
-    NSString *s1 = @"012345678901234567890123456789";
-    const char *utfS1 = [s1 UTF8String];
-    NSData *d1 = [NSData dataWithBytes:utfS1 length:strlen(utfS1)];
-    
-    // Test Binary encrypt/decrypt:
-    NSData *encryptedData = encryptBytesRaw(pin, d1);
-    NSData *decryptedData = decryptBytesRaw(pin, encryptedData);
-    assertT2Test(![encryptedData isEqualToData:d1], testDescription ); // Make sure encryped is different
-    assertT2Test([decryptedData isEqualToData:d1], testDescription ); // Make sure reconstructed is same
-    
-    // ------------------------------------------
-    // Test 7 - Test key/value interface
-    // ------------------------------------------
-    testDescription = startTest(@"Test 7.0 Test raw string encrypt/decrypt");
-    initializeCrypto(); // Initialize module
-    
-    // Now test out key/value interface
-    NSString *value = @"Some value";
-    NSString *value1 = @"Some other value";
-    NSString *key1 = @"Some key";
-    NSString *key2 = @"Some  other key";
-    
-    encryptedSaveValueForKey( pin, value, key1);
-    encryptedSaveValueForKey( pin, value1, key2);
+        // ------------------------------------------
+        // Test 1 - Test we can enter/edit FIP mode
+        // ------------------------------------------
+        testDescription = startTest(@"Test 1.0 Confirm FIP mode turned on");
+        retVal = FIPS_mode_set(1 /*on*/);
+        int fipsMode = FIPS_mode();
+        assertT2Test(retVal == T2True, testDescription );
+        assertT2Test(fipsMode == T2FipsModeOn, testDescription );
+        
+        testDescription = startTest(@"Test 1.1 Confirm FIP mode turned off");
+        retVal = FIPS_mode_set(0 /*off*/);
+        fipsMode = FIPS_mode();
+        assertT2Test(retVal == T2True, testDescription );
+        assertT2Test(fipsMode == T2FipsModeOff, testDescription );
+        
+        testDescription = startTest(@"Test 1.2 Confirm FIP mode turned on");
+        retVal = FIPS_mode_set(1 /*on*/);
+        fipsMode = FIPS_mode();
+        assertT2Test(retVal == T2True, testDescription );
+        assertT2Test(fipsMode == T2FipsModeOn, testDescription );
+        
+            
+        
+        // Make sure that the UI switch agrees with the current FIP mode
+        const int mode = FIPS_mode();
+        const BOOL state = mode ? YES : NO;
+        [m_modeSwitch setOn:state];
 
-//    NSLog(@"INFO: Saved value: \"%@\" with key: \"%@\" ",value, key1);
-//    NSLog(@"INFO: Saved value: \"%@\" with key: \"%@\" ",value, key2);
-//    NSLog(@"");
-    
-    NSString *recalledValue = encryptedGetValueForKey(pin, key1);
-    assertT2Test([recalledValue isEqualToString:value], testDescription );
+        // ------------------------------------------
+        // Test 2 - Test low level encrypt routines (AES_encrypt....
+        // ------------------------------------------
+        testLowLevelEncryptDecrypt();
+        
+        // ------------------------------------------
+        // Test 3,4 - Test raw string encrypt/descypt
+        // ------------------------------------------
+        
+        // Note that we intintially don't call initializeCrypto() here - these routines are stand alone
+        // and need no initialization
+        testDescription = startTest(@"Test 3.0 Test raw string encrypt/decrypt");
+        NSString *plainText = @"This is a test string to encrypt";
+        NSString *pin = (@"This is a funky $$$ pin");
+        
+        NSString *encryptedText = encryptRaw(pin, plainText);
+        NSString *decryptedText = decryptRaw(pin, encryptedText);
+        assertT2Test(![encryptedText isEqualToString:plainText], testDescription ); // Make sure encryped is different
+        assertT2Test([decryptedText isEqualToString:plainText], testDescription );  // Make sure reconstructed is same
+        
+        testDescription = startTest(@"Test 4.0 Test raw string encrypt/decrypt - blank string, blank pin");
+        plainText = @"";
+        pin = (@"");
+        
+        encryptedText = encryptRaw(pin, plainText);
+        decryptedText = decryptRaw(pin, encryptedText);
+        assertT2Test(![encryptedText isEqualToString:plainText], testDescription ); // Make sure encryped is different
+        assertT2Test([decryptedText isEqualToString:plainText], testDescription ); // Make sure reconstructed is same
+        
+        
+        // ------------------------------------------
+        // Test 5/6 - Test raw binary encrypt/decrypt
+        // ------------------------------------------
+        testDescription = startTest(@"Test 5,6 Test raw binary encrypt/decrypt");
+        pin = @"password";
+        NSString *s1 = @"012345678901234567890123456789";
+        const char *utfS1 = [s1 UTF8String];
+        NSData *d1 = [NSData dataWithBytes:utfS1 length:strlen(utfS1)];
+        
+        // Test Binary encrypt/decrypt:
+        NSData *encryptedData = encryptBytesRaw(pin, d1);
+        NSData *decryptedData = decryptBytesRaw(pin, encryptedData);
+        assertT2Test(![encryptedData isEqualToData:d1], testDescription ); // Make sure encryped is different
+        assertT2Test([decryptedData isEqualToData:d1], testDescription ); // Make sure reconstructed is same
+        
+        // ------------------------------------------
+        // Test 7 - Test key/value interface
+        // ------------------------------------------
+        testDescription = startTest(@"Test 7.0 Test raw string encrypt/decrypt");
+        initializeCrypto(); // Initialize module
+        
+        // Now test out key/value interface
+        NSString *value = @"Some value";
+        NSString *value1 = @"Some other value";
+        NSString *key1 = @"Some key";
+        NSString *key2 = @"Some  other key";
+        
+        encryptedSaveValueForKey( pin, value, key1);
+        encryptedSaveValueForKey( pin, value1, key2);
 
-    recalledValue = encryptedGetValueForKey(pin, key2);
-    assertT2Test([recalledValue isEqualToString:value1], testDescription );
+    //    NSLog(@"INFO: Saved value: \"%@\" with key: \"%@\" ",value, key1);
+    //    NSLog(@"INFO: Saved value: \"%@\" with key: \"%@\" ",value, key2);
+    //    NSLog(@"");
+        
+        NSString *recalledValue = encryptedGetValueForKey(pin, key1);
+        assertT2Test([recalledValue isEqualToString:value], testDescription );
 
-//    NSLog(@"INFO: Recalled value: \"%@\" with key \"%@\" ",recalledValue, key1);
-//    NSLog(@"INFO: Recalled value: \"%@\" with key \"%@\" ",recalledValue, key2);
+        recalledValue = encryptedGetValueForKey(pin, key2);
+        assertT2Test([recalledValue isEqualToString:value1], testDescription );
+
+    //    NSLog(@"INFO: Recalled value: \"%@\" with key \"%@\" ",recalledValue, key1);
+    //    NSLog(@"INFO: Recalled value: \"%@\" with key \"%@\" ",recalledValue, key2);
+        
+        // ------------------------------------------
+        // Test 8 - Test SqlCipher
+        // ------------------------------------------
+        testSqlCipher();
+        
+        NSLog(@"-------------------------------------------------------------");
+        NSLog(@"  Iteration %d", iterations);
+        NSLog(@"-------------------------------------------------------------");
+        
+        NSLog(@"   Iteration %d  - Tests passed: %d ", iterations, testPassCount);
+        NSLog(@"   Iteration %d  - Tests failed: %d ", iterations, testFailCount);
+        
+        
+        if (iterations++ >= NUM_ITERATIONS_TO_PERFORM)
+            break;
+        
+        
+    }
+
     
-    // ------------------------------------------
-    // Test 8 - Test SqlCipher
-    // ------------------------------------------
-    testSqlCipher();
+
+
+    if (testPassCount == testTotalCount) {
+        NSLog(@"");
+        NSLog(@"   ++ ALL TESTS PASSED ++ ");
+       // updateView("PASSED - All t2Crypto tests");
+    }
+    else {
+        NSLog(@"");
+        NSLog(@"   ********************** ONE OF MORE t2Crypto FAILED! ************************* ");
+        //updateView("** ONE OF MORE t2Crypto FAILED! **");
+    }
     
+    // ----------------
+    // Place any tests here that only should run once (regardless of what NUM_ITERATIONS_TO_PERFORM says
+    // ----------------
     
     // ------------------------------------------
     // Test 9 - Test file encryption - you have to listen for this!
@@ -373,6 +411,7 @@ void DisplayErrorMessage(const char* msg, unsigned long err)
             AVAudioPlayer *player = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL error:nil];
             //  player.numberOfLoops = -1; //Infinite
             [player play];
+            assertT2Test(TRUE, testDescription );
         } else {
             assertT2Test(FALSE, testDescription );
             NSLog(@"**** Error with output file");
@@ -383,32 +422,7 @@ void DisplayErrorMessage(const char* msg, unsigned long err)
     }
     
     
-    NSLog(@"-------------------------------------------------------------");
-    NSLog(@"  Iteration %d", iterations);
-    NSLog(@"-------------------------------------------------------------");
-
-    NSLog(@"   Iteration %d  - Tests passed: %d ", iterations, testPassCount);
-    NSLog(@"   Iteration %d  - Tests failed: %d ", iterations, testFailCount);
-
-    if (testPassCount == testTotalCount) {
-        NSLog(@"   ++ ALL TESTS PASSED ++ ");
-       // updateView("PASSED - All t2Crypto tests");
-    }
-    else {
-        NSLog(@"   ** ONE OF MORE t2Crypto FAILED! ** ");
-        //updateView("** ONE OF MORE t2Crypto FAILED! **");
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+  
 }
 
 - (void)didReceiveMemoryWarning
@@ -480,36 +494,61 @@ void testSqlCipher() {
                      initWithString: [docsDir stringByAppendingPathComponent:
                                       @"contacts.db"]];
     
+    NSLog(@"DB located at %@", databasePath);
+    
     NSFileManager *filemgr = [NSFileManager defaultManager];
     
+    
+    // Create the database file if not alredy there
     if ([filemgr fileExistsAtPath: databasePath ] == NO) {
+        
         const char *dbpath = [databasePath UTF8String];
         
         if (sqlite3_open(dbpath, &contactDB) == SQLITE_OK) {
             
-                                const char* key = [@"StrongPassword" UTF8String];
-                                sqlite3_key(contactDB, key, (int)strlen(key));
+            const char* key = [@"StrongPassword" UTF8String];
+            sqlite3_key(contactDB, key, (int)strlen(key));
             
-            char *errMsg;
-            const char *sql_stmt =
-            "CREATE TABLE IF NOT EXISTS CONTACTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ADDRESS TEXT, PHONE TEXT)";
-            
-            if (sqlite3_exec(contactDB, sql_stmt, NULL, NULL, &errMsg) != SQLITE_OK) {
-                NSLog(@"Failed to create table");
-                assertT2Test(NO, testDescription );
+            if (sqlite3_exec(contactDB, (const char*) "SELECT count(*) FROM sqlite_master;", NULL, NULL, NULL) == SQLITE_OK) {
+                NSLog(@"Password is correct, or a new database has been initialized");
+            } else {
+                NSLog(@"Incorrect password!");
             }
             sqlite3_close(contactDB);
-        } else {
-            NSLog(@"Failed to open/create database");
-            assertT2Test(NO, testDescription );
+ 
         }
     }
     
-    
+    // Create a table in the database
     sqlite3_stmt    *statement;
     const char *dbpath = [databasePath UTF8String];
     
     if (sqlite3_open(dbpath, &contactDB) == SQLITE_OK) {
+        
+        const char* key = [@"StrongPassword" UTF8String];
+        sqlite3_key(contactDB, key, (int)strlen(key));
+        
+        char *errMsg;
+        const char *sql_stmt =
+        "CREATE TABLE IF NOT EXISTS CONTACTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ADDRESS TEXT, PHONE TEXT)";
+        
+        if (sqlite3_exec(contactDB, sql_stmt, NULL, NULL, &errMsg) != SQLITE_OK) {
+            NSLog(@"Failed to create table");
+            assertT2Test(NO, testDescription );
+        }
+        sqlite3_close(contactDB);
+    } else {
+        NSLog(@"Failed to open/create database");
+        
+
+        sqlite3_close(contactDB);
+    }
+    
+    // Add A contact to the database
+    if (sqlite3_open(dbpath, &contactDB) == SQLITE_OK) {
+        
+        const char* key = [@"StrongPassword" UTF8String];
+        sqlite3_key(contactDB, key, (int)strlen(key));
         
         NSString *insertSQL = [NSString stringWithFormat:
                                @"INSERT INTO CONTACTS (name, address, phone) VALUES (\"%@\", \"%@\", \"%@\")",
@@ -528,13 +567,13 @@ void testSqlCipher() {
         sqlite3_finalize(statement);
         sqlite3_close(contactDB);
     }
-    
-    
-    //const char *dbpath = [_databasePath UTF8String];
-    //sqlite3_stmt    *statement;
 
-    
+    // Recall the contact from the database
     if (sqlite3_open(dbpath, &contactDB) == SQLITE_OK) {
+        
+        const char* key = [@"StrongPassword" UTF8String];
+        sqlite3_key(contactDB, key, (int)strlen(key));
+        
         NSString *querySQL = [NSString stringWithFormat:
                               @"SELECT name, address, phone FROM contacts WHERE name=\"%@\"",
                               dbValueName];
